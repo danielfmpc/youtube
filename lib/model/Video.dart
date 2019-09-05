3081b50d5e7550c2ded1151c83b03bb89fa6695c
Daniel Fernando <daniel_fmpc@hotmail.com.br>
2019-09-05 11:17:53 -0300

    Recuperando dados do YT

--- lib/model/Video.dart
+++ lib/model/Video.dart
@@ -3,5 +3,26 @@ class Video {
   String id, titulo, imagem, canal, descricao;
 
   Video({this.id, this.titulo, this.descricao, this.imagem, this.canal});
+  /*
+  static converterJson(Map<String, dynamic> json){
+    return Video(
+      id: json["id"]["videoId"],
+      titulo: json["snippet"]["titulo"],
+      imagem: json["snippet"]["thumbnails"]["high"]["url"],
+      canal: json["snippet"]["channelId"],
+
+    );
+  }
+  */
+
+  factory Video.fromJson(Map<String, dynamic> json){
+    return Video(
+      id: json["id"]["videoId"],
+      titulo: json["snippet"]["title"],
+      imagem: json["snippet"]["thumbnails"]["high"]["url"],
+      canal: json["snippet"]["channelId"],
+
+    );
+  }
 
 }
